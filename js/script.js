61d6377781db5c7b6209894dd792b9d6ae064b3d
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2018-05-28 09:02:45 +0200

    Improve code JS and HTML

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@
 /************* MUSTACHE pętla *****************/
 var listItems = '';
 var templateItem = document.getElementById('template-product-item').innerHTML;
-Mustache.parse(templateItem);
+//Mustache.parse(templateItem);
 
 for(var i = 0; i < carouselData.length; i++){
 		console.log(carouselData);
 		listItems += Mustache.render(templateItem, carouselData[i]);
 	};
 
-var fullProductList = Mustache.render(listItems);
+//var fullList = Mustache.render(listItems);
 
-results.insertAdjacentHTML('beforeend', fullProductList);
+results.insertAdjacentHTML('beforeend', listItems);
 
 
 
@@ -24,11 +24,6 @@ var flkty = new Flickity( elem, {
   pageDots: false,
   hash: true
 
-});
-
-
-var flkty = new Flickity( '.main-carousel', {
-  
 });
 
 var progressBar = document.querySelector('.progress-bar')
