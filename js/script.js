3252eb7e41bbfed4c526d7e42df1b3d263c757a6
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2018-06-03 12:25:20 +0200

    Link flickty and maps

--- js/script.js
+++ js/script.js
@@ -42,17 +42,85 @@ window.initMap = function() {
     center: carouselData[0].coords
   });
   
+
+  function createfunc(i) {
+    return function() { flkty.select(i); };
+};
+
+
+ var marker =[];
   for(var i = 0; i < carouselData.length; i++) {
-    var marker = new google.maps.Marker({
+    marker[i] = new google.maps.Marker({
       position: carouselData[i].coords,
       map: map     
     });
-    marker.addListener('click', function(){
-
-     location.href = "#carousel-cell1";
-      
-    });
+    marker[i].addListener('click', createfunc(i));
   }
+
+
+  flkty.on('change', function(index) {
+          smoothPanAndZoom(map, 8, carouselData[index].coords);
+          //map.panTo(carouselData[index].coords);
+          //map.setZoom(8);
+  });
 
 };
 
+
+
+/************** FUNKCJA SMOOTH ****************/
+  var smoothPanAndZoom = function(map, zoom, coords){
+    
+    var jumpZoom = zoom - Math.abs(map.getZoom() - zoom);
+    jumpZoom = Math.min(jumpZoom, zoom -1);
+    jumpZoom = Math.max(jumpZoom, 3);
+ 
+    smoothZoom(map, jumpZoom, function(){
+
+      smoothPan(map, coords, function(){
+        smoothZoom(map, zoom); 
+      });
+    });
+  };
+  
+  var smoothZoom = function(map, zoom, callback) {
+    var startingZoom = map.getZoom();
+    var steps = Math.abs(startingZoom - zoom);
+    
+    if(!steps) {
+      if(callback) {
+        callback();
+      }
+      return;
+    }
+
+    var stepChange = - (startingZoom - zoom) / steps;
+
+    var i = 0;
+    var timer = window.setInterval(function(){
+      if(++i >= steps) {
+        window.clearInterval(timer);
+        if(callback) {
+          callback();
+        }
+      }
+      map.setZoom(Math.round(startingZoom + stepChange * i));
+    }, 80);
+  };
+
+  var smoothPan = function(map, coords, callback) {
+    var mapCenter = map.getCenter();
+    coords = new google.maps.LatLng(coords);
+
+    var steps = 12;
+    var panStep = {lat: (coords.lat() - mapCenter.lat()) / steps, lng: (coords.lng() - mapCenter.lng()) / steps};
+
+    var i = 0;
+    var timer = window.setInterval(function(){
+      if(++i >= steps) {
+        window.clearInterval(timer);
+        if(callback) callback();
+      }
+      map.panTo({lat: mapCenter.lat() + panStep.lat * i, lng: mapCenter.lng() + panStep.lng * i});
+    }, 1000/30);
+  }; 
